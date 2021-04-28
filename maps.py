13a9e4f55ad54518b6465956c2fd01d47c372cc1
Petik Péter <petik2peter@gmail.com>
2021-04-28 22:09:32 +0200

    proba3

--- maps.py
+++ maps.py
@@ -1,15 +1,15 @@
 # %load_ext autoreload
 # %autoreload 2
 import json
-def prep(body):                
-    body = body[body.find(b'\\n') + 2 : body.rfind(b'\\n')]
-    body = body.replace(b'\/', b'/')
-    body = bytes(body.decode('unicode_escape'), 'latin-1')
-    return body    
 def getEntries(harFileName):
     with open(harFileName, "r") as read_file:
         data = json.load(read_file)
     return data['log']['entries']        
+def prep(body):                
+    body = body[body.find(b'\\n') + 2 : body.rfind(b'\\n')]
+    body = body.replace(b'\/', b'/')
+    body = bytes(body.decode('unicode_escape'), 'latin-1')
+    return body       
 def getBusinessData(e, b): # e: entry, b: business index
     ebin = prep(e.encode('utf-8'))
     edata = json.loads(ebin.decode('utf-8'))
@@ -17,8 +17,8 @@ def getBusinessData(e, b): # e: entry, b: business index
         'name' :edata[0][1][b][14][11],
         'url': edata[0][1][b][14][7][0]
     }
-def sav(body):  # for testing
-    with open("maps.json", "wb") as f:
+def sav(jsonFileName, body):  # for testing
+    with open(jsonFileName, "wb") as f:
         f.write(body)   
    
 harFile = 'www.google.com_Archive [21-04-11 10-44-37].json'
@@ -28,22 +28,7 @@ x = getBusinessData(entries[1], 1)
 listOfKeys = list(x.keys())
 
 '''
-def prep(body):                
-    body = body[body.find(b'\\n') + 2 : body.rfind(b'\\n')]
-    body = body.replace(b'\/', b'/')
-    body = bytes(body.decode('unicode_escape'), 'latin-1')
-    return body    
-def getEntries(harFileName):
-    with open(harFileName, "r") as read_file:
-        data = json.load(read_file)
-    return data['log']['entries']        
-def getBusinessData(subs):
-    subbin = prep(subs.encode('utf-8'))
-    ???? innen folyt. json.loads(subbin.decode('utf-8'))
-def sav(body):
-    with open("maps.json", "wb") as f:
-        f.write(body)
-
+####
 with open('www.google.com_Archive [21-04-11 10-44-37].json', "r") as read_file:
     data = json.load(read_file)
 subs = data['log']['entries'][0]['response']['content']['text']  
